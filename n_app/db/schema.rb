7f3ce9aae6b80da12ccabcbd06fe88ed52e0310d
Kazuta Kurose <kazyxwvkuvwxyz@gmail.com>
2024-05-31 20:54:45 +0900

    changed the format to yera/month/day and deleted post_day from the table

--- n_app/db/schema.rb
+++ n_app/db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_070338) do
   create_table "activities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "post_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
