0bad508e656fcc4ebfafb72f64c9a4b1ded19e86
Kazuta Kurose <kazyxwvkuvwxyz@gmail.com>
2024-06-02 18:40:20 +0900

    added graduation year in the db and displayed on the member's show page

--- n_app/app/models/member.rb
+++ n_app/app/models/member.rb
@@ -21,9 +21,24 @@ class Member < ApplicationRecord
     current_year_last_two_digits -= 1 if Date.today.month < 4
     # 入学年から現在の年までの差を計算し、学年を求める
     grade = current_year_last_two_digits - enrollment_year + 1
+    # 卒業年
+    graduation_year = enrollment_year + 4
 
     # 4年を超える場合は 'OM' を返す
     grade > 4 ? 'OM' : grade
+  end
+
+  def calculate_graduation_year(student_id)
+    # 学籍番号から入学年の下二桁を取得
+    enrollment_year = student_id[2..3].to_i
+    # 現在の年の下二桁を取得
+    current_year_last_two_digits = Date.today.year % 100
+    # 現在の月が4月以前の場合、学年の計算で前年を使用
+    current_year_last_two_digits -= 1 if Date.today.month < 4
+    # 入学年から現在の年までの差を計算し、学年を求める
+    grade = current_year_last_two_digits - enrollment_year + 1
+    # 卒業年
+    graduation_year = enrollment_year + 4
   end
 
   PROGRAMMING_LANGUAGES = {
--- n_app/db/schema.rb
+++ n_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_043804) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_01_085249) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_043804) do
     t.string "grade"
     t.bigint "learning_programming_languages", default: 0
     t.text "intro"
+    t.integer "graduation_year"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["invitation_token"], name: "index_members_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_members_on_invited_by_id"
