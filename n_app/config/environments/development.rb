0be6b211b0c0a18bdc43f5c26198e3339704729d
nectlife <nectgrams.webapp.team.member@gmail.com>
2024-09-28 23:10:35 +0900

    hostの設定ををドメインに変更

--- n_app/config/environments/development.rb
+++ n_app/config/environments/development.rb
@@ -74,7 +74,8 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  host = 'localhost' #←先ほどの上のコードのhost名と合わせる
-  Rails.application.routes.default_url_options[:host] = host
+  #host = 'localhost' #←先ほどの上のコードのhost名と合わせる
+  #Rails.application.routes.default_url_options[:host] = host
+  config.hosts << "nect-life.com"
 
 end
--- n_app/db/schema.rb
+++ n_app/db/schema.rb
@@ -85,8 +85,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_062750) do
     t.integer "learning_programming_languages", default: 0
     t.text "intro"
     t.integer "department"
-    t.string "course"
     t.integer "graduation_year"
+    t.string "course"
     t.integer "member_role", default: 0
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["invitation_token"], name: "index_members_on_invitation_token", unique: true
