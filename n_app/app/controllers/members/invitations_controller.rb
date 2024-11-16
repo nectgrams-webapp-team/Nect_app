f69e53e0700832999b49ead1985732a8fbf620f6
Death Salad <ankhbhold@gmail.com>
2024-11-16 13:14:32 +0900

    Indent standardization

--- n_app/app/controllers/members/invitations_controller.rb
+++ n_app/app/controllers/members/invitations_controller.rb
@@ -1,30 +1,30 @@
 class Members::InvitationsController < Devise::InvitationsController
-  before_action :configure_permitted_parameters
+    before_action :configure_permitted_parameters
 
-  def after_invite_path_for(resource)
-    site_admins_members_manager_index_path
-  end
-
-  def update
-    super do |resource|
-      if resource.errors.empty?
-        # ユーザーをログインさせる
-        sign_in(resource)
-
-        # ユーザーの招待受け入れが成功し、エラーがなければ編集ページにリダイレクト
-        redirect_to edit_member_path(resource)
-      else
-        # エラーがある場合は、デフォルトの動作を維持（通常は再度招待ページをrenderする）
-        respond_with_navigational(resource) { render :edit }
-      end
-      return # 重要: コントローラーアクションをここで終了させる
+    def after_invite_path_for(resource)
+        site_admins_members_manager_index_path
     end
-  end
 
-  protected
+    def update
+        super do |resource|
+            if resource.errors.empty?
+                # ユーザーをログインさせる
+                sign_in(resource)
 
-  # Permit the new params here.
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name, :student_id])
-  end
+                # ユーザーの招待受け入れが成功し、エラーがなければ編集ページにリダイレクト
+                redirect_to edit_member_path(resource)
+            else
+                # エラーがある場合は、デフォルトの動作を維持（通常は再度招待ページをrenderする）
+                respond_with_navigational(resource) { render :edit }
+            end
+            return # 重要: コントローラーアクションをここで終了させる
+        end
+    end
+
+    protected
+
+    # Permit the new params here.
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name, :student_id])
+    end
 end
--- n_app/config/routes.rb
+++ n_app/config/routes.rb
@@ -1,48 +1,48 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  get 'homes/about', to: 'homes#about', as: 'about'
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: 'about'
 
-  devise_for :members, controllers: {
-      invitations: 'members/invitations',
-      registrations: 'members/registrations',
-  }
+    devise_for :members, controllers: {
+            invitations: 'members/invitations',
+            registrations: 'members/registrations',
+    }
 
-  namespace :api, path: 'api', format: :json do
-    namespace :v1 do
-      post '/activities/preview', to: 'activities#preview'
+    namespace :api, path: 'api', format: :json do
+        namespace :v1 do
+            post '/activities/preview', to: 'activities#preview'
+        end
     end
-  end
 
-  resources :members, only: [:index, :show, :edit, :update]
-  resources :activities
-  resources :teams
-  resources :team_members
+    resources :members, only: [:index, :show, :edit, :update]
+    resources :activities
+    resources :teams
+    resources :team_members
 
-  namespace :site_admins do
-    get 'index', to: "base#index", as: 'index'
-    resources :carousel_images, :event_histories, only: [:index, :create, :update, :destroy]
-    resources :members_manager, only: [:index, :destroy] do
-      member do
-        get 'resend_invitation'
-        patch 'grant_mod_status'
-        patch 'revoke_mod_status'
-        patch 'grant_admin_status'
-        patch 'revoke_admin_status'
-      end
-      collection do
-        get 'statistics', to: 'members_manager#statistics', as: 'statistics'
-        post 'increment_grade'
-        post 'decrement_grade'
-      end
+    namespace :site_admins do
+        get 'index', to: "base#index", as: 'index'
+        resources :carousel_images, :event_histories, only: [:index, :create, :update, :destroy]
+        resources :members_manager, only: [:index, :destroy] do
+            member do
+                get 'resend_invitation'
+                patch 'grant_mod_status'
+                patch 'revoke_mod_status'
+                patch 'grant_admin_status'
+                patch 'revoke_admin_status'
+            end
+            collection do
+                get 'statistics', to: 'members_manager#statistics', as: 'statistics'
+                post 'increment_grade'
+                post 'decrement_grade'
+            end
+        end
     end
-  end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+    # Defines the root path route ("/")
+    # root "posts#index"
 end
