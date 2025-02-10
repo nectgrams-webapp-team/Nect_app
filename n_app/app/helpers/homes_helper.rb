4618aa7ed3298fc3bd3ab5311a1733bfa41a62ed
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2025-02-10 16:05:51 +0900

    不要なファイルとjsコードを修正

--- n_app/app/helpers/homes_helper.rb
+++ n_app/app/helpers/homes_helper.rb
@@ -1,2 +1,5 @@
 module HomesHelper
+  def responsive_activity_image_tag(source, options = {})
+    image_tag(source, options.merge({ onload: "adjustImageSize(this)" }))
+  end
 end
--- n_app/app/javascript/controllers/index.js
+++ n_app/app/javascript/controllers/index.js
@@ -2,10 +2,8 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import {application} from "./application"
 
 import CoursesController from "./courses_controller"
-application.register("courses", CoursesController)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+application.register("courses", CoursesController)
