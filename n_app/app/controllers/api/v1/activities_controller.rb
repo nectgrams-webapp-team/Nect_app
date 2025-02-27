e820eb3ee69e26aef71eb7b99242d0b0b0b98db3
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2025-02-28 04:07:25 +0900

    画像のリサイズ処理を追加

--- n_app/app/controllers/api/v1/activities_controller.rb
+++ n_app/app/controllers/api/v1/activities_controller.rb
@@ -14,16 +14,20 @@ module Api
 
         # 挿入された画像を取得
         dragged_image = params[:image]
+        file_size = dragged_image.tempfile.size
 
         # 画像を圧縮
         compressed_image = MiniMagick::Image.open(dragged_image.tempfile.path)
-        compressed_image.quality "50"
+        if compressed_image.width > 840
+          compressed_image.resize "840x"
+        else
+          compressed_image.quality "50"
+        end
         compressed_image.format "jpeg"
 
         # `compressed_image` を IO オブジェクトに変換
         io = StringIO.new(compressed_image.to_blob)
         filename = "#{File.basename(dragged_image.original_filename, '.*')}.jpg"
-
         image = ActiveStorage::Blob.create_and_upload!(
           io: io,
           filename: filename
